9a721bbe50dbf4b52cb4c5707005dd62f22562fe
J3r3mC <j.corbeilla@outlook.fr>
2026-04-14 15:51:53 +0200

    feat(auth): add register and token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/require-await */
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
