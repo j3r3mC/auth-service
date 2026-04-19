40c299d6f9c5a1778d49b30631b13c9b89053fbc
J3r3mC <j.corbeilla@outlook.fr>
2026-04-19 11:58:05 +0200

    feat(auth): fix  tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,15 @@
+import { defineConfig } from 'vitest/config';
+
 export default defineConfig({
   test: {
     globals: true,
-    environment: "node",
-    include: ["test/**/*.ts", "tests/**/*.ts", "**/*.spec.ts", "**/*.test.ts"],
-    deps: {
-      inline: ["@nestjs"]
-    }
+    environment: 'node',
+    include: ['test/**/*.ts', 'tests/**/*.ts', '**/*.spec.ts', '**/*.test.ts'],
+    typecheck: {
+      tsconfig: './tsconfig.test.json',
+    },
   },
   resolve: {
-    conditions: ["node"]
-  }
+    conditions: ['node'],
+  },
 });
