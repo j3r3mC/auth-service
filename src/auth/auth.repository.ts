ae26b96faeb238178a5922312bcffdeb55d8962f
J3r3mC <j.corbeilla@outlook.fr>
2026-04-20 12:00:48 +0200

    feat(auth):test loggout  ok

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -1,44 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { RegisterDto } from '../../src/auth/dto/register.dto';
+import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
 export class AuthRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  createUser(dto: RegisterDto) {
+  createUser = (dto: RegisterDto) => {
     return this.prisma.user.create({
       data: {
         email: dto.email,
         password: dto.password,
       },
     });
-  }
+  };
 
-  findById(id: string) {
+  findById = (id: string) => {
     return this.prisma.user.findUnique({
       where: { id },
     });
-  }
+  };
 
-  findByEmail(email: string) {
+  findByEmail = (email: string) => {
     return this.prisma.user.findUnique({
       where: { email },
     });
-  }
+  };
 
-  // 🔥 Correction ici : accepte string | null
-  updateRefreshToken(userId: string, refreshTokenHash: string | null) {
+  updateRefreshToken = (userId: string, refreshTokenHash: string | null) => {
     return this.prisma.user.update({
       where: { id: userId },
       data: { refreshToken: refreshTokenHash },
     });
-  }
+  };
 
-  clearRefreshToken(userId: string) {
+  clearRefreshToken = (userId: string) => {
     return this.prisma.user.update({
       where: { id: userId },
       data: { refreshToken: null },
     });
-  }
+  };
 }
